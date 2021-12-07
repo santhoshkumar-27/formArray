351910ffca3636893fd6ac7f1a6fae81dc4cf58b
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-12-07 19:12:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormarrayComponent } from './formarray/formarray.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormarrayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule, 
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formarray/formarray.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-formarray',
+  templateUrl: './formarray.component.html',
+  styleUrls: ['./formarray.component.css']
+})
+export class FormarrayComponent implements OnInit {
+  userForm !: FormGroup
+  userList!: FormArray
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      userList: new FormArray([
+        new FormGroup({
+          'userName': new FormControl(''),
+          'userDetails': new FormControl(''),
+          'userNumber': new FormControl('')
+        }),
+        new FormGroup({
+          'userName': new FormControl(''),
+          'userDetails': new FormControl(''),
+          'userNumber': new FormControl('')
+        })
+      ]),
+    })
+  }
+  // this is function returns a formGroup with collection of formcontrols
+  // createUserList(): FormGroup {
+  //   return this.fb.group({
+  //     userName: '',
+  //     userDetails: '',
+  //     userNumber: ''
+  //   })
+  // }
+  // addUsers(): void {
+  //   this.userList = this.userForm.get('userList') as FormArray
+  //   this.userList.push(this.createUserList())
+  // }
+  // button(): void {
+  //   console.log(this.userForm)
+  // }
+}
